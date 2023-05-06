b6852ca1eb3d2f5924c15307f6c8408a56f55988
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-05-06 20:47:26 +0200

    need teacher's review

---
+++ NetworkingHW/Models/RickAndMorty.swift
@@ -0,0 +1,26 @@
+//
+//  RickAndMirty.swift
+//  NetworkingHW
+//
+//  Created by Сергей Захаров on 06.05.2023.
+//
+
+import Foundation
+
+struct CharacterResponse: Codable {
+    let results: [Character]
+}
+
+struct Character: Codable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let type: String
+    let gender: String
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
